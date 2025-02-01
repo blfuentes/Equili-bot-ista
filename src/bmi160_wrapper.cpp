1ef7254fc44c7899d91de12a88a15f154c1d0e29
blfuentes <blfuentes@mundo-r.com>
2025-02-01 19:42:40 +0100

    refactor bmi160 wrapper

--- src/bmi160_wrapper.cpp
+++ src/bmi160_wrapper.cpp
@@ -1,68 +1,5 @@
-
-#include <driver/spi_common.h>
-#include <driver/spi_master.h>
-#include <driver/gpio.h>
 #include <esp_log.h>
-
 #include <bmi160_wrapper.h>
-
-static const char *TAG = "bmi160_wrapper";
-static spi_device_handle_t spiHandle;
-
-int8_t bmi_read_spi(uint8_t dev_addr, uint8_t reg_addr, uint8_t *data, uint16_t len);
-int8_t bmi_write_spi(uint8_t dev_addr, uint8_t reg_addr, uint8_t *read_data, uint16_t len);
-void bmi_delay(uint32_t period);
-
-bmi160_dev bmi_init_spi()
-{
-    bmi160_dev dev;
-    spi_bus_config_t spiBus;
-    spi_device_interface_config_t spiIf;
-
-    memset(&dev, 0, sizeof(bmi160_dev));
-    memset(&spiBus, 0, sizeof(spi_bus_config_t));
-    memset(&spiHandle, 0, sizeof(spi_device_handle_t));
-    memset(&spiIf, 0, sizeof(spi_device_interface_config_t));
-
-    // Dev config
-    dev.intf = BMI160_SPI_INTF;
-    dev.read = bmi_read_spi;
-    dev.write = bmi_write_spi;
-    dev.delay_ms = bmi_delay;
-
-    // Bus config
-    spiBus.mosi_io_num = GPIO_NUM_23;
-    spiBus.miso_io_num = GPIO_NUM_19;
-    spiBus.sclk_io_num = GPIO_NUM_18;
-    spiBus.quadhd_io_num = -1;
-    spiBus.quadwp_io_num = -1;
-    spiBus.max_transfer_sz = 4092;
-    
-    //ESP_ERROR_CHECK(spi_bus_initialize(SPI3_HOST, &busSpi, SPI_DMA_DISABLED));
-    if (spi_bus_initialize(SPI3_HOST, &spiBus, SPI_DMA_DISABLED) != ESP_OK)
-    {
-        ESP_LOGE(TAG, "SPI Bus cannot be initialized");
-        return dev;
-    }
-    ESP_LOGI(TAG, "SPI Bus Initialized correctly");
-
-    // Interface config
-    spiIf.spics_io_num = GPIO_NUM_21; // Cambiame
-    spiIf.clock_speed_hz = 5 * 1000 * 1000; // 5Mhz
-    spiIf.mode = 0;
-    spiIf.queue_size = 10;
-    spiIf.address_bits = 8;
-    
-    //ESP_ERROR_CHECK(spi_bus_add_device(SPI3_HOST, &spiIf, &spiHandle));
-    if (spi_bus_add_device(SPI3_HOST, &spiIf, &spiHandle) != ESP_OK)
-    {
-        ESP_LOGE(TAG, "SPI Device cannot be added");
-        return dev;
-    }
-    ESP_LOGI(TAG, "SPI Device Added correctly");
-
-    return dev;
-}
 
 int8_t bmi_read_spi(uint8_t dev_addr, uint8_t reg_addr, uint8_t *data, uint16_t len)
 {
@@ -75,14 +12,14 @@ int8_t bmi_read_spi(uint8_t dev_addr, uint8_t reg_addr, uint8_t *data, uint16_t 
     spiTrans.rxlength = 8 * len;
     spiTrans.rx_buffer = data;
 
-    spi_device_acquire_bus(spiHandle, portMAX_DELAY);
-    if (spi_device_polling_transmit(spiHandle, &spiTrans) != ESP_OK)
+    spi_device_acquire_bus(Bmi160<Bmi160SpiConfig>::spiHandle, portMAX_DELAY);
+    if (spi_device_polling_transmit(Bmi160<Bmi160SpiConfig>::spiHandle, &spiTrans) != ESP_OK)
     {
         ESP_LOGE(TAG, "SPI Transaction ERROR");
-        return -1;
+        return BMI160_TRANSACTION_READ_FAILURE;
     }
     ESP_LOGI(TAG, "SPI Transaction OK");
-    spi_device_release_bus(spiHandle);
+    spi_device_release_bus(Bmi160<Bmi160SpiConfig>::spiHandle);
     
     return BMI160_OK;
 };
@@ -97,14 +34,14 @@ int8_t bmi_write_spi(uint8_t dev_addr, uint8_t reg_addr, uint8_t *read_data, uin
     spiTrans.addr = reg_addr;
     spiTrans.tx_buffer = read_data;
 
-    spi_device_acquire_bus(spiHandle, portMAX_DELAY);
-    if (spi_device_polling_transmit(spiHandle, &spiTrans) != ESP_OK)
+    spi_device_acquire_bus(Bmi160<Bmi160SpiConfig>::spiHandle, portMAX_DELAY);
+    if (spi_device_polling_transmit(Bmi160<Bmi160SpiConfig>::spiHandle, &spiTrans) != ESP_OK)
     {
         ESP_LOGE(TAG, "SPI Transaction ERROR");
-        return -1;
+        return BMI160_TRANSACTION_WRITE_FAILURE;
     }
     ESP_LOGI(TAG, "SPI Transaction OK");
-    spi_device_release_bus(spiHandle);
+    spi_device_release_bus(Bmi160<Bmi160SpiConfig>::spiHandle);
 
     return BMI160_OK;
 };
@@ -116,127 +53,49 @@ void bmi_delay(uint32_t period)
     vTaskDelay(pdMS_TO_TICKS(period));
 };
 
-int8_t bmi_configure(bmi160_dev *dev)
+template<>
+uint8_t Bmi160<Bmi160SpiConfig>::init(Bmi160SpiConfig config)
 {
-    int8_t rslt;
-
-    rslt = bmi160_init(dev);
-
-    if (rslt == BMI160_OK)
+    spiBus.mosi_io_num = config.mosi;
+    spiBus.miso_io_num = config.miso;
+    spiBus.sclk_io_num = config.sclk;
+    spiBus.quadhd_io_num = -1;
+    spiBus.quadwp_io_num = -1;
+    spiBus.max_transfer_sz = 4092;
+    
+    if (spi_bus_initialize(config.spidev, &spiBus, SPI_DMA_DISABLED) != ESP_OK)
     {
-        ESP_LOGI(TAG, "BMI160 initialization success !");
-        ESP_LOGI(TAG, "Chip ID 0x%X", dev->chip_id);
-
-        /* Select the Output data rate, range of accelerometer sensor */
-        dev->accel_cfg.odr = BMI160_ACCEL_ODR_1600HZ;
-        dev->accel_cfg.range = BMI160_ACCEL_RANGE_16G;
-        dev->accel_cfg.bw = BMI160_ACCEL_BW_NORMAL_AVG4;
-
-        /* Select the power mode of accelerometer sensor */
-        dev->accel_cfg.power = BMI160_ACCEL_NORMAL_MODE;
-
-        /* Select the Output data rate, range of Gyroscope sensor */
-        dev->gyro_cfg.odr = BMI160_GYRO_ODR_3200HZ;
-        dev->gyro_cfg.range = BMI160_GYRO_RANGE_2000_DPS;
-        dev->gyro_cfg.bw = BMI160_GYRO_BW_NORMAL_MODE;
-
-        /* Select the power mode of Gyroscope sensor */
-        dev->gyro_cfg.power = BMI160_GYRO_NORMAL_MODE;
-
-        /* Set the sensor configuration */
-        return bmi160_set_sens_conf(dev);
+        ESP_LOGE(TAG, "SPI Bus cannot be initialized");
+        return BMI160_INIT_BUS_FAILURE;
     }
-    else
+    ESP_LOGI(TAG, "SPI Bus Initialized correctly");
+
+    spiIf.spics_io_num = config.cs; 
+    spiIf.clock_speed_hz = config.speed;
+    spiIf.mode = 0;
+    spiIf.queue_size = 10;
+    spiIf.address_bits = 8;
+    
+    if (spi_bus_add_device(config.spidev, &spiIf, &spiHandle) != ESP_OK)
     {
-        ESP_LOGE(TAG,"BMI160 initialization failure !");
-        return -1;
+        ESP_LOGE(TAG, "SPI Device cannot be added");
+        return BMI160_INIT_DEVICE_FAILURE;
     }
-};
+    ESP_LOGI(TAG, "SPI Device Added correctly");
 
-int8_t bmi160_get_sensor_data_adjusted(
-    uint8_t select_sensor, struct bmi160_sensor_data_wrapper *accel_wrapped, struct bmi160_sensor_data_wrapper *gyro_wrapped, const struct bmi160_dev *dev, bool adjusted)
+    
+    dev.intf = BMI160_SPI_INTF;
+    dev.read = bmi_read_spi;
+    dev.write = bmi_write_spi;
+    dev.delay_ms = bmi_delay;
+
+
+    return configure();
+}
+
+template<>
+uint8_t Bmi160<Bmi160I2cConfig>::init(Bmi160I2cConfig config)
 {
-    // Accelerator
-    float accel_factor = 16.0f;
-    switch(dev->accel_cfg.range) 
-    {
-        case BMI160_ACCEL_RANGE_2G:
-            accel_factor = 2.0f;
-            break;
 
-        case BMI160_ACCEL_RANGE_4G:
-            accel_factor = 4.0f;
-            break;
-
-        case BMI160_ACCEL_RANGE_8G:
-            accel_factor = 8.0f;
-            break;
-
-        case BMI160_ACCEL_RANGE_16G:
-            accel_factor = 16.0f;
-            break;
-    }
-    float accToG = accel_factor / float((1 << 15) - 1);
-
-    // Gyroscope
-    float gyro_factor = 2000.0f;
-    switch (dev->gyro_cfg.range)
-    {
-        case BMI160_GYRO_RANGE_125_DPS:
-            gyro_factor = 125.0f;
-            break;
-
-        case BMI160_GYRO_RANGE_250_DPS:
-            gyro_factor = 250.0f;
-            break;
-
-        case BMI160_GYRO_RANGE_500_DPS:
-            gyro_factor = 500.0f;
-            break;
-
-        case BMI160_GYRO_RANGE_1000_DPS:
-            gyro_factor = 1000.0f;
-            break;
-        
-        case BMI160_GYRO_RANGE_2000_DPS:
-            gyro_factor = 2000.0f;
-            break;
-    }
-    float gyroToDps = gyro_factor / float((1 << 15) - 1);
-
-    // Read data
-    bmi160_sensor_data accel;
-    bmi160_sensor_data gyro;
-
-    if (bmi160_get_sensor_data(BMI160_ACCEL_SEL | BMI160_GYRO_SEL, &accel, &gyro, dev) == BMI160_OK)
-    {
-        accel_wrapped->raw_data.x = accel.x;
-        accel_wrapped->raw_data.y = accel.y;
-        accel_wrapped->raw_data.z = accel.z;
-        accel_wrapped->raw_data.sensortime = accel.sensortime;
-
-        gyro_wrapped->raw_data.x = gyro.x;
-        gyro_wrapped->raw_data.y = gyro.y;
-        gyro_wrapped->raw_data.z = gyro.z;
-        gyro_wrapped->raw_data.sensortime = gyro.sensortime;
-
-        if (adjusted) 
-        {
-            accel_wrapped->adj_data.x = accel.x * accToG;
-            accel_wrapped->adj_data.y = accel.y * accToG;
-            accel_wrapped->adj_data.z = accel.z * accToG;
-            accel_wrapped->adj_data.sensortime = accel.sensortime;
-
-            gyro_wrapped->adj_data.x = gyro.x * gyroToDps;
-            gyro_wrapped->adj_data.y = gyro.y * gyroToDps;
-            gyro_wrapped->adj_data.z = gyro.z * gyroToDps;
-            gyro_wrapped->adj_data.sensortime = gyro.sensortime;
-        }
-
-        return BMI160_OK;
-    }
-    else
-    {
-        return -1;
-    }
+    return configure();
 };
--- src/main.cpp
+++ src/main.cpp
@@ -5,33 +5,41 @@
 #include <bmi160_wrapper.h>
 
 // Logger tag for ESP-IDF logging
-static const char *TAG = "app_main";
+static const char * MAIN_TAG = "app_main";
 
 extern "C" void app_main();
 
+Bmi160<Bmi160SpiConfig> imu;
+
 void app_main(void)
 {
-    bmi160_dev dev = bmi_init_spi();
+    vTaskDelay(pdMS_TO_TICKS(1000));
 
-    if (bmi_configure(&dev) != BMI160_OK)
+    Bmi160SpiConfig config = {
+        .spidev = SPI3_HOST,
+        .mosi = GPIO_NUM_23,
+        .miso = GPIO_NUM_19,
+        .sclk = GPIO_NUM_18,
+        .cs   = GPIO_NUM_21,
+        .speed= 4*1000*1000
+    };
+
+    if (imu.init(config) != BMI160_OK)
     {
-        ESP_LOGE(TAG, "Failed to configure bmi160 device");
+        ESP_LOGE(MAIN_TAG, "Failed to initialize bmi160 device");
     }
     else
     {
-
-        bmi160_sensor_data_wrapper accel_data;
-        bmi160_sensor_data_wrapper gyro_data;
-
-        for (;;)
+        Bmi160Data accel_data, gyro_data;
+        for(;;)
         {
-            if (bmi160_get_sensor_data_adjusted(BMI160_ACCEL_SEL | BMI160_GYRO_SEL, &accel_data, &gyro_data, &dev, true) == BMI160_OK)
-            {
-                printf("Raw-Accl: %6d %6d %6d\n", accel_data.raw_data.x, accel_data.raw_data.y, accel_data.raw_data.z);
-                printf("Adj-Accl: %.2f %.2f %.2f\n", accel_data.adj_data.x, accel_data.adj_data.y, accel_data.adj_data.z);
-                printf("Raw-Gyro: %6d %6d %6d\n", gyro_data.raw_data.x, gyro_data.raw_data.y, gyro_data.raw_data.z);
-                printf("Adj-Gyro: %.2f %.2f %.2f\n", gyro_data.adj_data.x, gyro_data.adj_data.y, gyro_data.adj_data.z);
-            }
+            imu.getData(accel_data, gyro_data);
+
+            printf("Raw-Accl: %6d %6d %6d\n", accel_data.raw_data.x, accel_data.raw_data.y, accel_data.raw_data.z);
+            printf("Adj-Accl: %.2f %.2f %.2f\n", accel_data.adj_data.x, accel_data.adj_data.y, accel_data.adj_data.z);
+            printf("Raw-Gyro: %6d %6d %6d\n", gyro_data.raw_data.x, gyro_data.raw_data.y, gyro_data.raw_data.z);
+            printf("Adj-Gyro: %.2f %.2f %.2f\n", gyro_data.adj_data.x, gyro_data.adj_data.y, gyro_data.adj_data.z);
+
             vTaskDelay(pdMS_TO_TICKS(1000));
         }
     }
