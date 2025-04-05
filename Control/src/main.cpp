89bcf0b9d82892b9999df88ed3e581d1c49c31ab
blfuentes <blfuentes@mundo-r.com>
2025-04-05 20:02:38 +0200

    removed unused files for control lib

--- Control/src/main.cpp
+++ Control/src/main.cpp
@@ -2,18 +2,19 @@
 #include <freertos/task.h>
 #include <driver/gpio.h>
 #include <driver/ledc.h>
-#include "driver/adc.h"
-#include "esp_adc_cal.h"
+#include "esp_adc/adc_oneshot.h"
+#include "esp_adc/adc_cali.h"
+#include "esp_adc/adc_cali_scheme.h"
 #include <esp_log.h>
 #include <esp_err.h>
 #include <memory>
 
-#include <MotorControl.h>
-#include <bmi160_wrapper.h>
-#include <PIDService.h>
+#include "PinDefinition.h"
 
 // Logger tag for ESP-IDF logging
-static const char *MAIN_TAG = "app_control_main";
+static const char *MAIN_TAG = "main_log";
+static const char *ADC_TAG = "adc_log";
+static const char *PARAMS_TAG = "params_log";
 
 enum class ModeType : u_int8_t {
     STANDING = 0,
@@ -40,12 +41,15 @@ auto current_param = std::make_unique<ParamType>(ParamType::P);
 PinGPIODefinition param;
 
 // Control
-constexpr gpio_num_t CONTROL_PIN = GPIO_NUM_2;
-#define POTENTIOMETER_ADC_CHANNEL   ADC1_CHANNEL_0  // GPIO1 on ESP32-C3
-#define ADC_WIDTH_BIT               ADC_WIDTH_BIT_12  // 12-bit resolution (0-4095)
-#define ADC_ATTEN_DB               ADC_ATTEN_DB_11   // Full-scale voltage ~3.3V
+constexpr gpio_num_t CONTROL_PIN = GPIO_NUM_0;
+#define POT_ADC_CHANNEL     ADC_CHANNEL_0  // GPIO36 if using ESP32-WROOM
+#define ADC_WIDTH           ADC_WIDTH_BIT_12
+#define ADC_ATTEN          ADC_ATTEN_DB_12  // 0-3.1V range
 
-//
+static int adc_raw[2][10];
+static int voltage[2][10];
+static bool example_adc_calibration_init(adc_unit_t unit, adc_channel_t channel, adc_atten_t atten, adc_cali_handle_t *out_handle);
+static void example_adc_calibration_deinit(adc_cali_handle_t handle);
 
 extern "C" void app_main();
 
@@ -83,16 +87,24 @@ void app_main(void)
     // Configuring PARAM PIN
     param = PinGPIODefinition(PARAM_PIN, GPIO_MODE_INPUT, GPIO_PULLDOWN_DISABLE);
     param.Configure();
-    // // Configuring CONTROL PIN
-    // // Configure ADC
-    // adc1_config_width(ADC_WIDTH_BIT);
-    // adc1_config_channel_atten(POTENTIOMETER_ADC_CHANNEL, ADC_ATTEN_DB);
+    // Configure ADC
+    //-------------ADC1 Init---------------//
+    adc_oneshot_unit_handle_t adc1_handle;
+    adc_oneshot_unit_init_cfg_t init_config1 = {
+        .unit_id = ADC_UNIT_1,
+    };
+    ESP_ERROR_CHECK(adc_oneshot_new_unit(&init_config1, &adc1_handle));
 
-    // // Characterize ADC (for better accuracy)
-    // esp_adc_cal_characteristics_t adc_chars;
-    // esp_adc_cal_characterize(ADC_UNIT_1, ADC_ATTEN_DB, ADC_WIDTH_BIT, 1100, &adc_chars);  // 1100mV default Vref
+    //-------------ADC1 Config---------------//
+    adc_oneshot_chan_cfg_t config = {
+        .atten = ADC_ATTEN,
+        .bitwidth = ADC_BITWIDTH_DEFAULT,
+    };
+    ESP_ERROR_CHECK(adc_oneshot_config_channel(adc1_handle, POT_ADC_CHANNEL, &config));
 
-
+    //-------------ADC1 Calibration Init---------------//
+    adc_cali_handle_t adc1_cali_chan0_handle = NULL;
+    bool do_calibration1_chan0 = example_adc_calibration_init(ADC_UNIT_1, POT_ADC_CHANNEL, ADC_ATTEN, &adc1_cali_chan0_handle);
     
     for (;;)
     {
@@ -113,22 +125,61 @@ void app_main(void)
         }
         ESP_LOGI(MAIN_TAG, "Current Mode: %s - Current Param: %s.", mode_to_string(*current_mode), param_to_string(*current_param));
 
-        // vTaskDelay(pdMS_TO_TICKS(100));
-        // if (param_value == 1) {
-        //     printf("Param is pressed...\n");
-        // } else {
-        //     printf("Param is released...\n");
-        // }
-
-        // // test control pin
-        // // Read raw ADC value (0-4095)
-        // int raw_value = adc1_get_raw(POTENTIOMETER_ADC_CHANNEL);
-
-        // // Convert to voltage (mV)
-        // int voltage_mV = esp_adc_cal_raw_to_voltage(raw_value, &adc_chars);
-
-        // printf("Raw ADC: %d, Voltage: %dmV\n", raw_value, voltage_mV);
+        ESP_ERROR_CHECK(adc_oneshot_read(adc1_handle, POT_ADC_CHANNEL, &adc_raw[0][0]));
+        ESP_LOGI(ADC_TAG, "ADC%d Channel[%d] Raw Data: %d", ADC_UNIT_1 + 1, POT_ADC_CHANNEL, adc_raw[0][0]);
+        if (do_calibration1_chan0) {
+            ESP_ERROR_CHECK(adc_cali_raw_to_voltage(adc1_cali_chan0_handle, adc_raw[0][0], &voltage[0][0]));
+            ESP_LOGI(ADC_TAG, "ADC%d Channel[%d] Cali Voltage: %d mV", ADC_UNIT_1 + 1, POT_ADC_CHANNEL, voltage[0][0]);
+        }
 
         vTaskDelay(pdMS_TO_TICKS(100));
     }
+
+    // Tear Down
+    ESP_ERROR_CHECK(adc_oneshot_del_unit(adc1_handle));
+    if (do_calibration1_chan0)
+    {
+        example_adc_calibration_deinit(adc1_cali_chan0_handle);
+    }
+}
+
+
+/*---------------------------------------------------------------
+        ADC Calibration
+---------------------------------------------------------------*/
+static bool example_adc_calibration_init(adc_unit_t unit, adc_channel_t channel, adc_atten_t atten, adc_cali_handle_t *out_handle)
+{
+    adc_cali_handle_t handle = NULL;
+    esp_err_t ret = ESP_FAIL;
+    bool calibrated = false;
+    if (!calibrated) {
+        ESP_LOGI(ADC_TAG, "calibration scheme version is %s", "Curve Fitting");
+        adc_cali_curve_fitting_config_t cali_config = {
+            .unit_id = unit,
+            .chan = channel,
+            .atten = atten,
+            .bitwidth = ADC_BITWIDTH_DEFAULT,
+        };
+        ret = adc_cali_create_scheme_curve_fitting(&cali_config, &handle);
+        if (ret == ESP_OK) {
+            calibrated = true;
+        }
+    }
+
+    *out_handle = handle;
+    if (ret == ESP_OK) {
+        ESP_LOGI(ADC_TAG, "Calibration Success");
+    } else if (ret == ESP_ERR_NOT_SUPPORTED || !calibrated) {
+        ESP_LOGW(ADC_TAG, "eFuse not burnt, skip software calibration");
+    } else {
+        ESP_LOGE(ADC_TAG, "Invalid arg or no memory");
+    }
+
+    return calibrated;
+}
+
+static void example_adc_calibration_deinit(adc_cali_handle_t handle)
+{
+    ESP_LOGI(ADC_TAG, "deregister %s calibration scheme", "Curve Fitting");
+    ESP_ERROR_CHECK(adc_cali_delete_scheme_curve_fitting(handle));
 }
